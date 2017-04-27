60167cd3614d4c3084812790313cba5d7ec07c10
Matt Bachmann <bachmann.matt@gmail.com>
2017-04-27 07:52:08 -0400

    Lets make some wheels

--- travisInstall.sh
+++ travisInstall.sh
@@ -5,7 +5,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install python3
     python3 -m venv venv
     source venv/bin/activate
+    pip install cython
 fi
 python -c "import sys; sys.exit(sys.version_info[:2] != (3,2))" 2>/dev/null || pip install -U pip wheel
-pip install --install-option="--no-cython-compile" -r requirements.txt
+
+# I took out "without cython. Unsure of the effect. I think it was breaking osx builds"
+pip install -r requirements.txt
 pip install -U beautifulsoup4 cssselect
