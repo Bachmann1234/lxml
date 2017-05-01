5e253a56df46b8c92d781cde476f705a24a0f1cb
Matt Bachmann <bachmann.matt@gmail.com>
2017-05-01 07:15:42 -0400

    Taking a shot at building manywheels on travisci

--- travis/travisRun.sh
+++ travis/travisRun.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -x -e
 
-if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-    eval "$(/usr/local/bin/pyenv init -)"
-    pyenv global "${PYENV_VERSION}"
-fi
 
-iconv --version
-python --version
-pip --version
-python -u setup.py clean
-CFLAGS="-O0 -g" python -u setup.py build_ext --inplace
-CFLAGS="-O0 -g" PYTHONUNBUFFERED=x make test
-python setup.py bdist_wheel
+if [ -z "${DOCKER_IMAGE}" ]; then
+
+    if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+        eval "$(/usr/local/bin/pyenv init -)"
+        pyenv global "${PYENV_VERSION}"
+    fi
+
+    iconv --version
+    python --version
+    pip --version
+    python -u setup.py clean
+    CFLAGS="-O0 -g" python -u setup.py build_ext --inplace
+    CFLAGS="-O0 -g" PYTHONUNBUFFERED=x make test
+
+    if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+        python setup.py bdist_wheel
+    fi
+else
+   make wheel_manylinux
+fi
