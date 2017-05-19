9b009121542cbda5a053bf1091060d4129cf4f60
Matt Bachmann <bachmann.matt@gmail.com>
2017-05-19 07:29:49 -0400

    Test this thign

--- travis/travisRun.sh
+++ travis/travisRun.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 set -x -e
-
-
-if [ -z "${DOCKER_IMAGE}" ]; then
-
-    if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-        eval "$(/usr/local/bin/pyenv init -)"
-        pyenv global "${PYENV_VERSION}"
-    fi
-
-    iconv --version
-    python --version
-    pip --version
-    python -u setup.py clean
-    CFLAGS="-O0 -g" python -u setup.py build_ext --inplace
-    CFLAGS="-O0 -g" PYTHONUNBUFFERED=x make test
-
-    if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-        make wheel_static
-    fi
-else
-   make wheel_manylinux
-fi
+cibuildwheel --output-dir wheelhouse
